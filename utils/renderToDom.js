8fa017d471029d182fede60ecf60c841dac5fe01
ahamm <ahammer753@gmail.com>
2023-03-03 07:30:29 -0600

    working on uid

--- utils/renderToDom.js
+++ utils/renderToDom.js
@@ -1,7 +1,6 @@
 const renderToDOM = (divId, content) => {
   const selectedDiv = document.querySelector(divId);
   selectedDiv.innerHTML = content;
-  console.log('content', content);
 };
 
 export default renderToDOM;
